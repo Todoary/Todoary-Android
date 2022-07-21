1fe46a5ff8cc3ca6439d186979c1249707724db3
Hyuns66 <hyuns6677@gmail.com>
2022-07-21 14:24:13 +0900

    [Feat] : Add Calendar View in MainActivity v 메인액티비티에 달력뷰 추가 세팅에서 물음표 눌렀을 때 다이얼로그 나오는 기능추가 +) 프로필 사진설정 추가 구현 필요

--- app/src/main/java/com/uni/todoary/feature/main/ui/MainActivity.kt
+++ app/src/main/java/com/uni/todoary/feature/main/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.uni.todoary.feature.main.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.uni.todoary.R
 import com.uni.todoary.base.BaseActivity
 import com.uni.todoary.databinding.ActivityMainBinding
 import com.uni.todoary.feature.setting.ui.SettingActivity
@@ -14,5 +15,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
             val menuIntent = Intent(this, SettingActivity::class.java)
             startActivity(menuIntent)
         }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_calendar_fl, CalendarFragment())
+            .commit()
     }
 }
